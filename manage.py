f59ec1d0ef1ef6b16e8e88620bd1c833d5ad9a7c
Fernando-00 <fernandorosell2024@u.northwestern.edu>
2023-07-05 23:25:01 -0500

    changed python version

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamechat.settings')
+    os.environ["PYTHON_VERSION"] = "3.9.13"
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
